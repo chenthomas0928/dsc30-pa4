5520b44ebed40d3b40922aa115fde8759bc70f3c
Thomas Chen <ychen@ucsd.edu>
2022-04-26 23:57:11 -0700

    PA4

---
+++ src/DLLQueueTest.java
@@ -0,0 +1,26 @@
+import junit.framework.TestCase;
+
+public class DLLQueueTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testSize() {
+        DLLQueue queue = new DLLQueue();
+        int size = 0;
+        assertEquals(size, queue.size());
+    }
+
+    public void testIsEmpty() {
+    }
+
+    public void testEnqueue() {
+    }
+
+    public void testDequeue() {
+    }
+
+    public void testPeek() {
+    }
+}
---
+++ src/DLLStackTest.java
@@ -0,0 +1,26 @@
+import junit.framework.TestCase;
+
+public class DLLStackTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testSize() {
+        DLLStack stack = new DLLStack();
+        int size = 0;
+        assertEquals(size, stack.size());
+    }
+
+    public void testIsEmpty() {
+    }
+
+    public void testPush() {
+    }
+
+    public void testPop() {
+    }
+
+    public void testPeek() {
+    }
+}
